aed43eb1594e6aaebde6c2b1d37cc73a3bdc5674
kocsist <ktomi87@gmail.com>
2020-09-22 16:19:36 +0200

    1. Feladat

--- zene/Program.cs
+++ zene/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace zene
 {
+    class adatok{
+       
+        public int radioSorszam;
+        public TimeSpan szamhossza;
+        public string szamEloado;
+        public string szamZeneszam;
+    }
     class Program
     {
+        public static List<adatok> musor = new List<adatok>();
+        public static int mennyiSzolt = 0;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            task1();
         }
+
+        private static void task1()
+        {
+            StreamReader sr = new StreamReader("musor.txt");
+            mennyiSzolt=(int.Parse(sr.ReadLine()));
+            
+            while (!sr.EndOfStream)
+            {
+                adatok a = new adatok();
+                string ujSor = sr.ReadLine();
+                string[] temp = ujSor.Split(" ");
+                a.radioSorszam = int.Parse(temp[0]);
+                a.szamhossza = TimeSpan.Parse("00:"+temp[1]+":"+temp[2]);
+                string szAzon="";
+                for (int i = 3; i < temp.GetLength(0); i++)
+                {
+                    szAzon += temp[i];
+                }
+                string[] temp2 = szAzon.Split(":");
+                a.szamEloado = temp2[0];
+                a.szamZeneszam = temp2[1];
+                musor.Add(a);
+            }
+
+            sr.Close();
+        }
+
     }
 }
