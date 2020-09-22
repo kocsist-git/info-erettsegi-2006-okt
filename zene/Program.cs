be760f6af63c2a2df2b4a91428b45acde6c71827
kocsist <ktomi87@gmail.com>
2020-09-22 15:14:04 +0200

    Add project files.

---
+++ zene/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
